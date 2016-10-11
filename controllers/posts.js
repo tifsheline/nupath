3a9aa18353724ca55000ef263656adb7e719394a
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-10-11 11:05:57 -0700

    Show post view

--- controllers/posts.js
+++ controllers/posts.js
@@ -43,11 +43,11 @@ module.exports = {
   },
 
   show: function(req, res) {
-    Post.findById(req.params.id, function(err, data) {
+    Post.findById(req.params.id).populate('_by').exec(function(err, data) {
       if (err) {
         res.json(err);
       } else {
-        res.json(data);
+        res.render('posts/show', {post: data});
       }
     });
   },
