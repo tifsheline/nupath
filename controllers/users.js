24d6d57ad2d70e6a4bed77e9b1a016354cead1e9
Anil Rayamajhi <anilrm07@gmail.com>
2016-10-12 16:15:15 -0700

    commenter is attached wih comment

--- controllers/users.js
+++ controllers/users.js
@@ -15,7 +15,7 @@ module.exports = {
   },
 
   show: function(req, res){
-  	User.findById(req.params.id).populate('posts').exec(function(err, data){
+  	User.findById(req.params.id).populate({path:'posts', populate:{path:'comments._by'} }).exec(function(err, data){
     	if (err) {
       	res.json(err);
       } else {
